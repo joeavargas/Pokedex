d1d892c7f82d72135527620b054aba87c4bb3761
Joe Vargas <joe.vargas.iosdev@gmail.com>
2022-09-06 23:32:17 -0500

    setup folder structure

---
+++ Pokedex/Screens/PokedexView.swift
@@ -0,0 +1,20 @@
+//
+//  PokedexView.swift
+//  Pokedex
+//
+//  Created by Joe Vargas on 9/6/22.
+//
+
+import SwiftUI
+
+struct PokedexView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokedexView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexView()
+    }
+}
